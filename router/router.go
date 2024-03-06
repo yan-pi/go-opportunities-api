4509c0762b508d4648d5584894249091f3c30241
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2024-03-06 00:19:45 -0300

    feat: add routes group api/v1 and create openings endpoint

--- router/router.go
+++ router/router.go
@@ -3,11 +3,11 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize(){
+	//Initialize gin router
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// Initialize routes
+	initializeRoutes(r)
+
 	r.Run(":8080") // listen and serve on
 }
