f4f38a29dc838566855d5da69229878a2728f1e2
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2024-03-09 18:24:50 -0300

    feat: add endpoints for openings

--- router/routes.go
+++ router/routes.go
@@ -9,9 +9,33 @@ import (
 func initializeRoutes(router *gin.Engine){
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Openings endpoint",
+		v1.GET("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Get Openings endpoint",
+			})
+		})
+
+		v1.POST("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Post Openings endpoint",
+			})
+		})
+
+		v1.DELETE("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Delete Openings endpoint",
+			})
+		})
+
+		v1.PUT("/opening", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Put Openings endpoint",
+			})
+		})
+
+		v1.GET("/list-openings", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Get All Openings endpoint",
 			})
 		})
 	}
