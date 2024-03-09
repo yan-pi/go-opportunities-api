cebb598e4f6f89f796c2dda36a1ba5847214b7a2
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2024-03-09 19:50:57 -0300

    feat and refactor: create endpointspackage handlers and adjust routes

--- router/routes.go
+++ router/routes.go
@@ -1,42 +1,21 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/Yan-pi/go-opportunities-api/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine){
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "Get Openings endpoint",
-			})
-		})
+		v1.POST("/opening", handler.CreateOpeningHandler)
 
-		v1.POST("/opening", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "Post Openings endpoint",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
 
-		v1.DELETE("/opening", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "Delete Openings endpoint",
-			})
-		})
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
 
-		v1.PUT("/opening", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "Put Openings endpoint",
-			})
-		})
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 
-		v1.GET("/list-openings", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "Get All Openings endpoint",
-			})
-		})
+		v1.GET("/list-openings", handler.ListOpeningHandler)
 	}
 }
