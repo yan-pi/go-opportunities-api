22ac4598b4727545d840c9f5db12663d27b2df85
yan-pi <102800044+yan-pi@users.noreply.github.com>
2024-03-12 18:21:52 -0300

    feat: add feature to verify db file/folder

--- config/sqlite.go
+++ config/sqlite.go
@@ -10,6 +10,29 @@ import (
 
 func InitializeSQLite() (*gorm.DB, error) {
 	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+	
+	// verifica se o db existe
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("database file does not exist, creating...")
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+
+	db, err := gorm.Open(sqlite.Open("./db/main.db"), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("sqlite opening error", err)
+		return nil, err
+	}
 
 	err = db.AutoMigrate(&schemas.Opening{})
 	if err != nil {
