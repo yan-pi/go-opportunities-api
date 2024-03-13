08758df6798f146875b5b0c9ab19b8f83ca4e7c2
yan-pi <102800044+yan-pi@users.noreply.github.com>
2024-03-13 16:06:11 -0300

    feat: create and initialize handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/Yan-pi/go-opportunities-api/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
--- router/routes.go
+++ router/routes.go
@@ -6,16 +6,14 @@ import (
 )
 
 func initializeRoutes(router *gin.Engine){
+	handler.InitializeHandler()
+	
 	v1 := router.Group("/api/v1")
 	{
 		v1.POST("/opening", handler.CreateOpeningHandler)
-
 		v1.GET("/opening", handler.ShowOpeningHandler)
-
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
-
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
-
 		v1.GET("/list-openings", handler.ListOpeningHandler)
 	}
 }
