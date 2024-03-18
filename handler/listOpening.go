9cf2f733387fcdadbcc54a6e46eb17610d138695
yan-pi <yan-pi@users.noreply.github.com>
2024-03-18 17:09:51 -0300

    feat: add docs for listOpening handler

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -3,10 +3,21 @@ package handler
 import (
 	"net/http"
 
-	"github.com/Yan-pi/go-opportunities-api/schemas"
 	"github.com/gin-gonic/gin"
+	"github.com/yan-pi/go-opportunities-api/schemas"
 )
 
+
+// @BasePath /api/v1
+
+// @Summary List openings
+// @Description List all job openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 
