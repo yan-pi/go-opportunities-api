f9b5fafd9181d816803fa562e65231567e673582
yan-pi <yan-pi@users.noreply.github.com>
2024-03-18 17:09:40 -0300

    feat: add docs for deleteOpening handler

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -4,10 +4,22 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/Yan-pi/go-opportunities-api/schemas"
 	"github.com/gin-gonic/gin"
+	"github.com/yan-pi/go-opportunities-api/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Delete opening
+// @Description Delete a new job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} DeleteOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [delete]
 func DeleteOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
