2a2f70fe61acf1c0f1f059d25334255eb4d7a889
yan-pi <yan-pi@users.noreply.github.com>
2024-03-18 16:08:16 -0300

    feat: showOpening

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -3,11 +3,24 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Yan-pi/go-opportunities-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Post Openings endpoint",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	//find
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	//show
+	sendSuccess(ctx, "show-opening", opening)
 }
