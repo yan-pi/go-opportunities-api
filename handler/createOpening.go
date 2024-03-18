cc3281f83a6d17f441a6d640a893985a675f571a
yan-pi <yan-pi@users.noreply.github.com>
2024-03-18 17:09:29 -0300

    feat: add docs for createOpening handler

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -3,10 +3,22 @@ package handler
 import (
 	"net/http"
 
-	"github.com/Yan-pi/go-opportunities-api/schemas"
 	"github.com/gin-gonic/gin"
+	"github.com/yan-pi/go-opportunities-api/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Create opening
+// @Description Create a new job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param request body CreateOpeningRequest true "Request body"
+// @Success 200 {object} CreateOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [post]
 func CreateOpeningHandler(ctx *gin.Context) {
 	request := CreateOpeningRequest{}
 
