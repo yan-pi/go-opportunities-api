222186237f4f412fd414debb44c963cf2ac3e269
yan-pi <yan-pi@users.noreply.github.com>
2024-03-18 16:07:58 -0300

    feat: add description on createOpening and add updateOpening request

--- handler/request.go
+++ handler/request.go
@@ -3,13 +3,13 @@ package handler
 import "fmt"
 
 type CreateOpeningRequest struct {
-	Role string `json:"role"`
-	//Description string `json:"description"`
-	Company  string `json:"company"`
-	Location string `json:"location"`
-	Remote   *bool `json:"remote"`
-	Link     string `json:"link"`
-	Salary   int64 `json:"salary"`
+	Role        string `json:"role"`
+	Description string `json:"description"`
+	Company     string `json:"company"`
+	Location    string `json:"location"`
+	Remote      *bool  `json:"remote"`
+	Link        string `json:"link"`
+	Salary      int64  `json:"salary"`
 }
 
 func errParamIsRequired(name, typ string) error {
@@ -17,11 +17,14 @@ func errParamIsRequired(name, typ string) error {
 }
 
 func (r *CreateOpeningRequest) Validate() error {
-	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Salary <= 0 {
+	if r.Role == "" && r.Description == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Salary <= 0 {
 		return fmt.Errorf("request body is empty or malformed")
 	}
 	if r.Role == "" {
 		return errParamIsRequired("role", "string")
+	}
+	if r.Description == "" {
+		return errParamIsRequired("description", "string")
 	}
 	if r.Company == "" {
 		return errParamIsRequired("company", "string")
@@ -39,4 +42,22 @@ func (r *CreateOpeningRequest) Validate() error {
 		return errParamIsRequired("salary", "int64")
 	}
 	return nil
+}
+
+type UpdateOpeningRequest struct {
+	Role        string `json:"role"`
+	Description string `json:"description"`
+	Company     string `json:"company"`
+	Location    string `json:"location"`
+	Remote      *bool  `json:"remote"`
+	Link        string `json:"link"`
+	Salary      int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Description != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Salary > 0 {
+		return nil
+	}
+
+	return fmt.Errorf("at least one valid field must be provided")
 }
