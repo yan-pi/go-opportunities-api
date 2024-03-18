234cb41ffbb6e74b4541912146b20acb9db99296
yan-pi <yan-pi@users.noreply.github.com>
2024-03-18 16:10:11 -0300

    feat: add swagger on routes

--- router/routes.go
+++ router/routes.go
@@ -1,14 +1,19 @@
 package router
 
 import (
+	docs "github.com/Yan-pi/go-opportunities-api/docs"
 	"github.com/Yan-pi/go-opportunities-api/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func initializeRoutes(router *gin.Engine){
+func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
-	
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath= basePath
+
+	v1 := router.Group(basePath)
 	{
 		v1.POST("/opening", handler.CreateOpeningHandler)
 		v1.GET("/opening", handler.ShowOpeningHandler)
@@ -16,4 +21,6 @@ func initializeRoutes(router *gin.Engine){
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.GET("/list-openings", handler.ListOpeningHandler)
 	}
+
+	router.GET("swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
